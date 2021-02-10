557f6694ebfaa26987ae0696a03a2d5556fce58b
Adelina9 <adelina.girlea@gmail.com>
2021-02-10 10:30:04 +0200

    Integrate Crashlytics correctly, extract string resources

--- app/src/main/java/com/example/mafia/presentation/LoginActivity.java
+++ app/src/main/java/com/example/mafia/presentation/LoginActivity.java
@@ -6,25 +6,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.mafia.BuildConfig;
 import com.example.mafia.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+
+import timber.log.Timber;
 
 public class LoginActivity extends AppCompatActivity {
     EditText editTextEmail, editTextPassword;
     Button buttonLogin;
     FirebaseAuth firebaseAuth;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        if (!BuildConfig.my_flag) {
+//            Timber.plant(new Timber.DebugTree());
+//            Timber.wtf("Nu e setat");
+//        } else {
+        Timber.plant(new Timber.Tree() {
+            @Override
+            protected void log(int priority, @Nullable String tag, @NotNull String message, @Nullable Throwable t) {
+                if (priority < Log.INFO) {
+                    return;
+                }
+
+                if (t != null) {
+                    FirebaseCrashlytics.getInstance().recordException(t);
+                }
+
+                String crashlyticsMessage = String.format("[%s] %s", tag, message);
+                FirebaseCrashlytics.getInstance().log(crashlyticsMessage);
+            }
+        });
+//        }
         setContentView(R.layout.activity_login);
 
         editTextEmail = findViewById(R.id.editTextLoginActivityEmailInput);
@@ -37,9 +69,22 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(), CreateRoomActivity.class));
             finish();
         }
+
+        Button crashButton = new Button(this);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                throw new RuntimeException("Test Crash"); // Force a crash
+            }
+        });
+
+        addContentView(crashButton, new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 
     public void loginExistingUser(View view) {
+        Timber.tag(TAG).w(new IOException(), "Something went wrong");
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
